691c051e93f4ef163156afaaaecda02e0f90f510
rvilela7 <ruivil@gmail.com>
2024-04-16 16:23:35 +0100

    .

---
+++ Learning/HelloWorld1.cbl
@@ -0,0 +1,7 @@
+000100 IDENTIFICATION DIVISION.
+       PROGRAM-ID. HelloWorld.
+       AUTHOR. Rui Vilela.
+       DATE-WRITTEN.  15/04/2024.
+       PROCEDURE DIVISION.
+           DISPLAY "Hello World!".
+           STOP RUN.
---
+++ Notas.md
@@ -0,0 +1,9 @@
+# Extensions VSCODE
+    - IBM Z Open Editos
+    - Zowe Explorer
+
+# Requirements
+    Java >11 64bits
+
+
+
